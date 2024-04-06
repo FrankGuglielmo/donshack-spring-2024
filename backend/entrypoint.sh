640045e9b807ee29ca1db54795ae29d70c0759c1
MalekeHan <Malekehanson@gmail.com>
2024-04-06 02:23:36 -0700

    moved docker files

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,10 +5,6 @@ set -e
 echo "Applying database migrations..."
 python manage.py migrate
 
-# Collect static files
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
-
 # Start Django app
 echo "Starting Django app..."
 python manage.py runserver 0.0.0.0:8000
