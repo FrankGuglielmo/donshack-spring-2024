c18e1265fe8dcd385be5150b0f9e12b2dcfdffab
laperzyna <90733086+laperzyna@users.noreply.github.com>
2024-04-06 01:51:23 -0700

    fixed routing

--- frontend/events_application/src/App.js
+++ frontend/events_application/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/HomePage";
 import ProfilePage from "./pages/ProfilePage";
+import EventPage from "./pages/EventPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/event" element={<EventPage />} />
         </Routes>
       </Router>
     </div>
--- frontend/events_application/src/components/Navbar.js
+++ frontend/events_application/src/components/Navbar.js
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useAuth0 } from "@auth0/auth0-react";
+import tempLogo from '../imgs/tempLogo.jpeg';
 
 export default function NavbarMain() {
   const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
@@ -9,7 +10,13 @@ export default function NavbarMain() {
     <>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#" style={{ height: "5%", width: "3%", backgroundColor: "#bbb", borderRadius: "50%" }}>Lg</Navbar.Brand>
+        <Navbar.Brand href="/">
+            <img
+              src={tempLogo}
+              alt="temp logo"
+              style={{ height: '50px', borderRadius: '30px' }}
+            />
+          </Navbar.Brand>
           <Nav className="justify-content-end">
             <Nav.Link>
               {/* If not already authenticated, generate a login button. Otherwise, logout. */}
