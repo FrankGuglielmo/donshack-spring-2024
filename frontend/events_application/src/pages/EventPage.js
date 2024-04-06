e0fead09906d0a6344d0de7371bddc2a3963872e
laperzyna <90733086+laperzyna@users.noreply.github.com>
2024-04-06 11:11:52 -0700

    event page p much done

--- frontend/events_application/src/pages/EventPage.js
+++ frontend/events_application/src/pages/EventPage.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import { useState, useEffect, useRef } from "react";
 import NavbarMain from '../components/Navbar';
 import Footer from '../components/Footer';
 import Button from 'react-bootstrap/Button';
+import Slideshow from '../components/Slideshow';
+import { FaArrowDown } from 'react-icons/fa';
 import "../styles/home.css";
 
 function EventPage() {
+    const eventGalleryRef = useRef(null);
+
+    const scrollToGallery = () => {
+        eventGalleryRef.current?.scrollIntoView({ behavior: 'smooth' });
+    };
+
     // TEST array of images
     const images = [
-        // 'https://via.placeholder.com/250',
-        // 'https://via.placeholder.com/250',
-        // 'https://via.placeholder.com/250'
+        'https://via.placeholder.com/250',
+        'https://via.placeholder.com/150',
+        'https://via.placeholder.com/350',
+        'https://via.placeholder.com/750',
+        'https://via.placeholder.com/150',
+        'https://via.placeholder.com/250',
+        'https://via.placeholder.com/450',
+        'https://via.placeholder.com/250'
     ];
 
     return (
@@ -17,18 +30,24 @@ function EventPage() {
             <header>
                 <NavbarMain />
             </header>
-            <section id="home-blurb">
-                <h2>DONS Hack 2024</h2>
-                <p>DONS Hack 2024 was an event associated with the University of San Francisco. It was hosted by two on-campus clubs, WIT & ACM. The event last from April 06, 2024 to April 08, 2024. This event was a Hackathon, which is a rigorous coding competition where students are given a short amount of time to develop some kind of application. Good Luck Students!</p>
-            </section>
-            <section id="event-gallery">
+            <div className="photo-grid-display">
+                <Slideshow images={images} />
+                <div className="overlay">
+                    <section id="home-blurb">
+                        <h2>DONS Hack 2024</h2>
+                        <br/>
+                        <p>DONS Hack 2024 was an event associated with the University of San Francisco. It was hosted by two on-campus clubs, WIT & ACM. The event last from April 06, 2024 to April 08, 2024. This event was a Hackathon, which is a rigorous coding competition where students are given a short amount of time to develop some kind of application. Good Luck Students!</p>
+                    </section>
+                    <button className="scroll-down-btn" onClick={scrollToGallery}>
+                        <FaArrowDown size={25} />
+                    </button>
+                </div>
+            </div>
+            <section id="event-gallery" ref={eventGalleryRef}>
                 <div className="d-flex justify-content-end">
                     <Button variant="primary" className="add-photo-btn">
                         Add Your Photos
                     </Button>
-                </div>
-                <div className="photo-grid">
-                    photo fade grid
                 </div>
                 <div className="photo-container">
                     <div className="photo-gallery">
