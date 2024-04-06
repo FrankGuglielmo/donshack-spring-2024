84c77f33f54cbdc4ae2c584075f2534beb698c22
MalekeHan <Malekehanson@gmail.com>
2024-04-06 01:22:23 -0700

    added viewsets

--- backend/events_application/serializers.py
+++ backend/events_application/serializers.py
@@ -1,10 +1,21 @@
 from rest_framework import serializers
 from .models import User, Event, MediaUpload
+from django.contrib.auth.hashers import make_password
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'name', 'password']  
+        fields = ['id', 'name', 'password']
+        extra_kwargs = {'password': {'write_only': True, 'required': True}} # make the passwords write only so they don't actually show up in the database for read/GET requests
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            name=validated_data['name'],
+            password=make_password(validated_data['password'])
+        )
+        return user
+
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
