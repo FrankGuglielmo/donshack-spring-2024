5579352c0cba8f532c4f230f5f0230c75ce39639
clarejw <cjwilson6@dons.usfca.edu>
2024-04-06 21:49:23 -0700

    nav bar dropdown (profile  accessible from home)

--- frontend/events_application/src/components/Navbar.js
+++ frontend/events_application/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import { useAuth0 } from "@auth0/auth0-react";
 import Button from "react-bootstrap/Button";
 import tempLogo from "../imgs/tempLogo.jpeg";
@@ -19,14 +20,47 @@ export default function NavbarMain() {
             />
           </Navbar.Brand>
           <Nav className="justify-content-end">
-            <Nav.Link>
-              {/* If not already authenticated, generate a login button. Otherwise, logout. */}
+            {/* <Nav.Link>
+              //If not already authenticated, generate a login button. Otherwise, logout. 
               {!isAuthenticated ? (
                 <Button onClick={() => loginWithRedirect()}>Log In</Button>
               ) : (
                 <Button onClick={() => logout()}>Log Out</Button>
               )}
-            </Nav.Link>
+            </Nav.Link> */}
+            <NavDropdown
+              className="dropdown bg-large"
+              title="☰"
+              id="basic-nav-dropdown"
+              align="end"
+              style={{ fontSize: "23px" }}
+            >
+              <NavDropdown.Item className="nav-item" href="/">
+                Home
+              </NavDropdown.Item>
+              {isAuthenticated && (
+                <NavDropdown.Item className="nav-item" href="/#/profile">
+                  Profile
+                </NavDropdown.Item>
+              )}
+
+              {/* If not already authenticated, generate a login button. Otherwise, logout. */}
+              {!isAuthenticated ? (
+                <NavDropdown.Item
+                  className="nav_item"
+                  onClick={() => loginWithRedirect({})}
+                >
+                  Log In/Sign Up
+                </NavDropdown.Item>
+              ) : (
+                <NavDropdown.Item
+                  className="nav_item"
+                  onClick={() => logout({})}
+                >
+                  Log Out
+                </NavDropdown.Item>
+              )}
+            </NavDropdown>
           </Nav>
         </Container>
       </Navbar>
